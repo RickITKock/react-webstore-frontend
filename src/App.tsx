0015fae37fd69fb5d6e0aa16075d4279aeb0f9ce
rick124 <rick@roosh.online>
2023-02-03 16:01:09 +0100

    Create basic form element

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,9 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from "react";
+import SignUpForm from "./components/SignUpForm";
 
-const App = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (username === "") {
-      alert("Username cannot be empty!");
-      return;
-    }
-
-    // Password must not be empty and must contain a number
-    // and should be at least 8 characters long
-    if (password.length === 0) {
-      alert("Password cannot be empty");
-      return;
-    } else if (!/\d/.test(password)) {
-      // Check if password contains a number
-      alert("Password must contain at least one number");
-      return;
-    } else if (password.length < 8) {
-      // Check if password is at least 8 characters long
-      alert("Password must be at least 8 characters long");
-      return;
-    }
-
-    alert("Done!");
-    // submit form data here...
-  };
-
-  return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <label>Username:</label>
-      <input
-        type="text"
-        value={username}
-        className="form-input"
-        onChange={(e) => setUsername(e.target.value)}
-      />
-
-      <label>Password:</label>
-      <input
-        type="password"
-        className="form-input"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <button type="submit" className="form-button">
-        Submit
-      </button>
-    </form>
-  );
-};
+// Extract the form in this file and turn it into a component
+function App() {
+  return <SignUpForm />;
+}
 
 export default App;
---
+++ src/components/SignUpForm.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    alert("Submitting...");
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label>Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+export default App;
